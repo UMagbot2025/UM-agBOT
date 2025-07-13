fe5d6f2cfa453ac4c80a22aba71f89c3fd99aec3
UMagbot2025 <umanitobaagbot@gmail.com>
2025-07-13 13:52:08 -0500

    added abstraction: Krish-Bhalala

--- led.ino
+++ led.ino
@@ -1,21 +1,36 @@
 // Simple function to turn any LED on or off by pin number
 
+#define RED 13
+#define GREEN 8
+
 void setLed(int pin, bool state) {
   digitalWrite(pin, state ? HIGH : LOW);
 }
 
 void setup() {
   // Example: using pins 13 and 8 for LEDs
-  pinMode(13, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+}
+
+void goodEgg(){
+  setLed(GREEN, true);
+  delay(1000);
+  setLed(GREEN, false);
+}
+
+void badEgg(){
+  setLed(REN, true);
+  delay(1000);
+  setLed(RED, false);
 }
 
 void loop() {
-  setLed(13, true);  // Turn ON LED at pin 13
-  setLed(8, false);  // Turn OFF LED at pin 8
+  setLed(RED, true);  // Turn ON LED at pin 13
+  setLed(GREEN, false);  // Turn OFF LED at pin 8
   delay(1000);
 
-  setLed(13, false); // Turn OFF LED at pin 13
-  setLed(8, true);   // Turn ON LED at pin 8
+  setLed(RED, false); // Turn OFF LED at pin 13
+  setLed(GREEN, true);   // Turn ON LED at pin 8
   delay(1000);
 }
